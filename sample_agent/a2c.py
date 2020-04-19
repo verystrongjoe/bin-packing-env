9fdbca3ce19249abbfa9fd409c0944b7c210231b
unknown <verystrongjoe@gmail.com>
2020-04-20 07:52:08 +0900

    a2c

--- sample_agent/a2c.py
+++ sample_agent/a2c.py
@@ -1,11 +1,11 @@
 """
 https://github.com/ethancaballero/pytorch-a2c-ppo/blob/master/main.py
 """
-
 import argparse
 import gym
 import numpy as np
 from itertools import count
+from torch.autograd import Variable
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -15,7 +15,10 @@ import env_config as env_cfg
 from environment import PalleteWorld
 from tensorboardX import SummaryWriter
 
-num_frames = 1000
+
+torch.autograd.set_detect_anomaly(True)
+
+num_frames = 10000000
 num_steps = 10
 num_processes = 10
 seed = 0
@@ -112,7 +115,7 @@ class A2C(nn.Module):
             for m in p[:step]:
                 action_probs[i][m] = -np.inf
 
-        return F.softmax(action_probs, dim=1), value
+        return value, F.softmax(action_probs, dim=1)
 
 
 policy = A2C()
@@ -121,7 +124,7 @@ eps = np.finfo(np.float32).eps.item()
 optimizer = optim.RMSprop(policy.parameters(), lr=1e-2, eps=eps , alpha=0.99)
 
 
-def select_action(state, previous_action, step):
+def calibrate_state(state):
     # this is for visual representation.
     boxes = torch.zeros(num_processes, 10, 10, env_cfg.ENV.BIN_MAX_COUNT)  # x, y, box count
     for i, p in enumerate(state):  # box = x,y
@@ -132,52 +135,62 @@ def select_action(state, previous_action, step):
     np.asarray([state[i][1] for i in range(num_steps)])[:,:,:,0:1],
     boxes), axis=-1)
 
-    state = torch.from_numpy(state).float().cuda()
-    probs, values  = policy(state, previous_action, step)
-    ps = probs.cpu().data.numpy()
+    return torch.from_numpy(state).float().cuda()
+
+
+def select_action(state, previous_action, step):
+    value, prob = policy(state, previous_action, step)
+    ps = prob.cpu().data.numpy()
     actions = []
-    action_probs = []
-    for p in ps:
+    logits = []
+    entropy = []
+    for i, p in enumerate(ps):
         action = np.random.choice(env_cfg.ENV.BIN_MAX_COUNT, 1, p=p)[0]
         actions.append(action)
-        action_prob = torch.log(probs[0][action])
-        action_probs.append(action_prob)
+        logit = torch.log(prob[i][action])
+        logits.append(logit)
+        e = -np.sum(np.mean(p) * np.log(p+1e5))  # todo : p + epsilon?
+        entropy.append(e)
 
-    return actions, action_probs, values
+    return actions, logits, value, entropy
 
 
 def main():
-    obs_shape = (10, 10, env_cfg.ENV.BIN_MAX_COUNT+1)
-    states = torch.zeros(num_steps + 1, num_processes, *obs_shape)
-    current_state = torch.zeros(num_processes, *obs_shape)
 
-    state = envs.reset()
 
-    rewards = torch.zeros(num_steps, num_processes, 1)
-    value_preds = torch.zeros(num_steps + 1, num_processes, 1)
-    returns = torch.zeros(num_steps + 1, num_processes, 1)
-    actions = torch.LongTensor(num_steps, num_processes)
-    masks = torch.zeros(num_steps, num_processes, 1)
-    old_log_probs = torch.zeros(num_steps, num_processes, num_steps)
-
-    episode_rewards = torch.zeros([num_processes, 1])
-    final_rewards = torch.zeros([num_processes, 1])
-
-    states = states.cuda()
-    current_state = current_state.cuda()
-    rewards = rewards.cuda()
-    value_preds = value_preds.cuda()
-    old_log_probs = old_log_probs.cuda()
-    returns = returns.cuda()
-    actions = actions.cuda()
-    masks = masks.cuda()
 
     """
     ****************************에피소드 실행 메인**************************************
     """
     for j in range(num_updates):  # total episode
+        # print('episode {} started..'.format(j))
 
-        previous_action = np.zeros((num_processes, num_steps))
+        obs_shape = (10, 10, env_cfg.ENV.BIN_MAX_COUNT + 1)
+        states = torch.zeros(num_steps + 1, num_processes, *obs_shape)
+        state = envs.reset()
+        state = calibrate_state(state)
+
+        rewards = torch.zeros(num_steps, num_processes, 1)
+        value_preds = torch.zeros(num_steps + 1, num_processes, 1)
+        returns = torch.zeros(num_steps + 1, num_processes, 1)
+        actions = torch.zeros(num_steps, num_processes)
+        masks = torch.zeros(num_steps, num_processes, 1)
+        log_probs = torch.zeros(num_steps, num_processes)
+        ents = torch.zeros(num_steps, num_processes)
+        episode_rewards = torch.zeros([num_processes, 1])
+        final_rewards = torch.zeros([num_processes, 1])
+
+        states = states.cuda()
+        rewards = rewards.cuda()
+        values = value_preds.cuda()
+        log_probs = log_probs.cuda()
+        returns = returns.cuda()
+        actions = actions.cuda()
+        masks = masks.cuda()
+        ents = ents.cuda()
+
+
+        previous_action = np.zeros((num_processes, num_steps), dtype=np.int)
 
         for step in range(num_steps):  # episode step
 
@@ -187,40 +200,68 @@ def main():
             # log_probs = F.log_softmax(logits).data
             # actions[step] = probs.multinomial().data  # todo : multinomial sampling
 
-            actions, action_probs, values = select_action(state, previous_action, step)
-            log_probs = action_probs
+            actions, logits, value, entropy = select_action(state, previous_action, step)
 
-            actions = [env_cfg.Action(bin_index=a, priority=1, rotate=0) for a in actions]
+            env_actions = [env_cfg.Action(bin_index=a, priority=1, rotate=0) for a in actions]
 
-            state, reward, done, info = envs.step(actions)
+            state, reward, done, info = envs.step(env_actions)
+            state = calibrate_state(state)
 
             for i, a in enumerate(actions):
                 previous_action[i][step] = a
 
             reward = torch.from_numpy(np.expand_dims(np.stack(reward), 1)).float()
-            episode_rewards += reward
+            episode_rewards += reward  # (num_process, reward_sum)
 
             np_masks = np.array([0.0 if done_ else 1.0 for done_ in done])  # (process, done)
-            n_placed_items = len(info['placed_items'])
 
             # If done then clean the history of observations.
             pt_masks = torch.from_numpy(np_masks.reshape(np_masks.shape[0], 1, 1, 1)).float()
             pt_masks = pt_masks.cuda()
-            current_state *= pt_masks
 
-            states[step + 1].copy_(current_state)
-            value_preds[step].copy_(values.data)
-            old_log_probs[step].copy_(log_probs)
+            state *= pt_masks
+            states[step + 1].copy_(state)
+            values[step].copy_(value)
+            log_probs[step].copy_(torch.stack(logits))
+            ents[step].copy_(torch.from_numpy(np.asarray(entropy)))  # entropies = (num_step, process)
             rewards[step].copy_(reward)
+
             masks[step].copy_(torch.from_numpy(np_masks).unsqueeze(1))
 
-            final_rewards *= masks[step].cpu()
-            final_rewards += (1 - masks[step].cpu()) * episode_rewards
-
+            final_rewards = final_rewards * masks[step].cpu()
+            final_rewards = final_rewards + (1 - masks[step].cpu()) * episode_rewards
             episode_rewards *= masks[step].cpu()
 
-            # writer.add_scalar('data/final_reward', ep_reward, i_episode)
-            # writer.add_scalar('data/n_placed_items', n_placed_items, i_episode)
+        # writer.add_scalar('data/final_reward', np.mean(episode_rewards), j)
+
+        # 여기에선 종료 여부가 done이 True가 나오면, 그 다음 state를 받아서 다시 policy에서 action은 취하지 않고 value만  가져옴
+        returns[-1] = policy(Variable(states[-1]), masks=previous_action, step=step)[0]
+
+        for step in reversed(range(num_steps)):  # 10 9 8 7
+            returns[step] = returns[step + 1] * 0.99 * masks[step] + rewards[step]
+
+        advantages = returns[:-1] - values[:-1]
+        # advantages = returns - values
+        value_loss = advantages.pow(2).mean()
+        action_loss = -(advantages * log_probs).mean()
+
+        optimizer.zero_grad()
+        (value_loss * 0.5 + action_loss - torch.sum(ents) * 0.01).backward(retain_graph=True)
+
+        nn.utils.clip_grad_norm(policy.parameters(), 0.5)
+        optimizer.step()
+
+        states[0].copy_(states[-1])
+
+        if j % 10 == 0:
+            print("Updates {}, num frames {}, mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
+                format(j, j * num_processes * num_steps,
+                       final_rewards.mean(),
+                       final_rewards.median(),
+                       final_rewards.min(),
+                       final_rewards.max(), -torch.sum(ents),
+                       value_loss.item(), action_loss.item()))
+
 
 if __name__ == '__main__':
     main()
